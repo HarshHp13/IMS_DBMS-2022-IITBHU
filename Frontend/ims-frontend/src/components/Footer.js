23b5c69c98a58e99c9e175200ef43d235c792682
Harshit <harshit.singh.cse20@itbhu.ac.in>
2022-10-31 06:21:51 +0530

    footer

--- Frontend/ims-frontend/src/components/Footer.js
+++ Frontend/ims-frontend/src/components/Footer.js
@@ -1,67 +1,56 @@
-import {Input, Button} from '@mui/material'
+import { Input, Button } from '@mui/material'
 import React from 'react'
 import './Footer.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faYoutube,
-    faFacebook,
-    faTwitter,
-    faInstagram
-  } from "@fortawesome/free-brands-svg-icons";
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram
+} from "@fortawesome/free-brands-svg-icons";
+
+import { Link } from 'react-router-dom';
 
 function Footer() {
+  let scrollToTop = ()=>{
+    // console.log("function to call ho rha hai...");
+    // window.scrollBy({
+    //   top: 0,
+    //   left: 0,
+    //   behavior: 'smooth'
+    // });
+    window.scrollTo(0, 0);
+  }
+
   return (
-    <>
-
-        <div className="container">
-             <div className="tookre-1">
-                <h3 className='lst'>About Us</h3>
-                <h3 className='lst'>Policies</h3>
-                <h3 className='lst'>Contact Us</h3>
-             </div>
-
-             <div className="tookre-2">
-                
-                <div className="tookre-2a">
-                    <h3>+91 7217213319</h3>
-                    <h3>mishra.x.dhruv18@gmail.com</h3>
-                    <h4>Vishwesaraya hostel, Near Hyderabad gate, IIT BHU-Varanasi</h4>
-                    <img src="" alt="" />
-                </div>
-                
-                <div className="tookre-2b">
-                    <a href="https://www.youtube.com/c/jamesqquick"
-                        className="youtube social">
-                        <FontAwesomeIcon icon={faYoutube} size="4x" />
-                    </a>
-                    <a href="https://www.facebook.com/learnbuildteach/"
-                        className="facebook social">
-                        <FontAwesomeIcon icon={faFacebook} size="4x" />
-                    </a>
-                    <a href="https://www.twitter.com/jamesqquick" className="twitter social">
-                        <FontAwesomeIcon icon={faTwitter} size="4x" />
-                    </a>
-                    <a href="https://www.instagram.com/learnbuildteach"
-                        className="instagram social">
-                        <FontAwesomeIcon icon={faInstagram} size="4x" />
-                    </a>
-                </div>
-                
-             </div>
-             <div className="tookre-3">
-                <h1>Write to us :</h1>
-                 <form className="form" action="">
-                   <input type="text" className='name' placeholder='Your Name'/>
-                    <br />
-                   <input type="email" className='email' placeholder='Email'/>
-                   <textarea name="" id="" cols="30" rows="10" placeholder='Your Concern '></textarea>
-                   <br />
-                   <input type="submit" className='btn'/>
-                 </form>
-             </div>
-        </div>
-    </>
-  )
-} 
+    <div className='Footer__container'>
+      <div className='Footer__contact'>
+        <div className='Footer__phone'>9910279337</div>
+        <div className='Footer__phone'>9910279227</div>
+        <div className='Footer__email'>help@ims.ac.in</div>
+        <div className='Footer__address'>Vishwesaraya hostel, Near Hyderabad gate, IIT BHU-Varanasi</div>
+      </div>
+      <div className="Footer__bottomLine Footer__topLine">
+        <a href="https://www.youtube.com/c/jamesqquick" className="Footer__youtube Footer__social">
+          <FontAwesomeIcon icon={faYoutube} size="3x" />
+        </a>
+        <a href="https://www.facebook.com/learnbuildteach/" className="Footer__facebook Footer__social">
+          <FontAwesomeIcon icon={faFacebook} size="3x" />
+        </a>
+        <a href="https://www.twitter.com/jamesqquick" className="Footer__twitter Footer__social">
+          <FontAwesomeIcon icon={faTwitter} size="3x" />
+        </a>
+        <a href="https://www.instagram.com/learnbuildteach" className="Footer__instagram Footer__social">
+          <FontAwesomeIcon icon={faInstagram} size="3x" />
+        </a>
+      </div>
+      <div className='Footer__links'>
+        <div className='Footer__home Footer__link' onClick={scrollToTop}><Link className='link' to={'/'}>Home</Link></div>
+        <div className='Footer__policies Footer__link' onClick={scrollToTop}><Link className='link' to={'/policies'}>Policies</Link></div>
+        <div className='Footer__aboutUs Footer__link' onClick={scrollToTop}><Link className='link' to={'/about'}>About Us</Link></div>
+      </div>
+    </div>
+  );
+}
 export default Footer;
 
