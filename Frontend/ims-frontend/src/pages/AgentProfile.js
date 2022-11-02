8fd67a41c4ba90693e2730991be16c081cdde1fa
Harshit <harshit.singh.cse20@itbhu.ac.in>
2022-11-02 08:44:46 +0530

    profileAndPayment

--- Frontend/ims-frontend/src/pages/AgentProfile.js
+++ Frontend/ims-frontend/src/pages/AgentProfile.js
@@ -1,146 +1,154 @@
 import React from 'react';
 import './AgentProfile.css'
 
-function AdminProfile() {
+function AgentProfile() {
     const totalStars = 5;
-    const activeStars = 3.6;
+    const activeStars = 4.3;
 
     return (
-        <div className='AdminProfile__container'>
-            <div className='AdminProfile__top AdminProfile_bottomBorder'>
-                <div className='AdminProfile__admin'>
-                    <div className='AdminProfile__picture'></div>
-                    <div className='AdminProfile__name'>Harshit Singh</div>
+        <div className='AgentProfile__container'>
+            <div className='AgentProfile__top AgentProfile_bottomBorder'>
+                <div className='AgentProfile__agent'>
+                    <div className='AgentProfile__picture'></div>
+                    <div className='AgentProfile__name'>Harshit Singh</div>
                 </div>
-                <div className='AdminProfile__stars'>
-                    Rating : 4.3
+                <div className='AgentProfile__stars'>
+                    Rating : {activeStars}
                 </div>
             </div>
-            <div className='AdminProfile__adminData AdminProfile_bottomBorder'>
-                <div className='AdminProfile__dataBlock'>
-                    <div className='AdminProfile__left_dataBlock'>
+            <div className='AgentProfile__agentData AgentProfile_bottomBorder'>
+                <div className='AgentProfile__dataBlock'>
+                    <div className='AgentProfile__left_dataBlock'>
                         <strong>username:</strong>
                     </div>
-                    <div className='AdminProfile__right_dataBlock AdminProfile__username'>
+                    <div className='AgentProfile__right_dataBlock AgentProfile__username'>
                         ArtofHarry7
                     </div>
                 </div>
-                <div className='AdminProfile__dataBlock'>
-                    <div className='AdminProfile__left_dataBlock'>
+                <div className='AgentProfile__dataBlock'>
+                    <div className='AgentProfile__left_dataBlock'>
                         <strong>Email:</strong>
                     </div>
-                    <div className='AdminProfile__right_dataBlock AdminProfile__email'>
+                    <div className='AgentProfile__right_dataBlock AgentProfile__email'>
                         artofharry00@gmail.com
                     </div>
                 </div>
-                <div className='AdminProfile__dataBlock'>
-                    <div className='AdminProfile__left_dataBlock'>
+                <div className='AgentProfile__dataBlock'>
+                    <div className='AgentProfile__left_dataBlock'>
                         <strong>Date of Birth:</strong>
                     </div>
-                    <div className='AdminProfile__right_dataBlock AdminProfile__dateOfBirth'>
+                    <div className='AgentProfile__right_dataBlock AgentProfile__dateOfBirth'>
                         18th January, 2000
                     </div>
                 </div>
-                <div className='AdminProfile__dataBlock'>
-                    <div className='AdminProfile__left_dataBlock'>
+                <div className='AgentProfile__dataBlock'>
+                    <div className='AgentProfile__left_dataBlock'>
                         <strong>Age:</strong>
                     </div>
-                    <div className='AdminProfile__right_dataBlock AdminProfile__age'>
+                    <div className='AgentProfile__right_dataBlock AgentProfile__age'>
                         22
                     </div>
                 </div>
-                <div className='AdminProfile__dataBlock'>
-                    <div className='AdminProfile__left_dataBlock'>
+                <div className='AgentProfile__dataBlock'>
+                    <div className='AgentProfile__left_dataBlock'>
                         <strong>Referrals:</strong>
                     </div>
-                    <div className='AdminProfile__right_dataBlock AdminProfile__referrals'>
+                    <div className='AgentProfile__right_dataBlock AgentProfile__referrals'>
                         4
                     </div>
                 </div>
-                <div className='AdminProfile__dataBlock'>
-                    <div className='AdminProfile__left_dataBlock'>
+                <div className='AgentProfile__dataBlock'>
+                    <div className='AgentProfile__left_dataBlock'>
                         <strong>phone:</strong>
                     </div>
-                    <div className='AdminProfile__right_dataBlock AdminProfile__phone'>
+                    <div className='AgentProfile__right_dataBlock AgentProfile__phone'>
                         9910279337
                     </div>
                 </div>
-                <div className='AdminProfile__dataBlock'>
-                    <div className='AdminProfile__left_dataBlock'>
+                <div className='AgentProfile__dataBlock'>
+                    <div className='AgentProfile__left_dataBlock'>
                         <strong>Address:</strong>
                     </div>
-                    <div className='AdminProfile__right_dataBlock AdminProfile__address'>
+                    <div className='AgentProfile__right_dataBlock AgentProfile__address'>
                         House no. 154<br />
                         street no. 8, tigaon Road<br />
                         near RK-Tower<br />
                         Faridabad, Haryana - 121005
                     </div>
                 </div>
-                <div className='AdminProfile__dataBlock'>
-                    <div className='AdminProfile__left_dataBlock'>
+                <div className='AgentProfile__dataBlock'>
+                    <div className='AgentProfile__left_dataBlock'>
                         <strong>Profession:</strong>
                     </div>
-                    <div className='AdminProfile__right_dataBlock AdminProfile__profession'>
+                    <div className='AgentProfile__right_dataBlock AgentProfile__profession'>
                         Software Developer
                     </div>
                 </div>
-                <div className='AdminProfile__dataBlock'>
-                    <div className='AdminProfile__left_dataBlock'>
+                <div className='AgentProfile__dataBlock'>
+                    <div className='AgentProfile__left_dataBlock'>
                         <strong>Income:</strong>
                     </div>
-                    <div className='AdminProfile__right_dataBlock AdminProfile__income'>
+                    <div className='AgentProfile__right_dataBlock AgentProfile__income'>
                         13.6 lakh PA
                     </div>
                 </div>
             </div>
-            <div className='AdminProfile__policyData AdminProfile_bottomBorder'>
+            <div className='AgentProfile__policyData AgentProfile_bottomBorder'>
                 <h3>Your Clients</h3>
-                <div className='AdminProfile__dataBlock'>
-                    <div className='AdminProfile__left_dataBlock'>
+                <div className='AgentProfile__dataBlock'>
+                    <div className='AgentProfile__left_dataBlock'>
                         <strong>Complete Policies:</strong>
                     </div>
-                    <div className='AdminProfile__right_dataBlock AdminProfile__completePolicies'>
+                    <div className='AgentProfile__right_dataBlock AgentProfile__completePolicies'>
                         3
                     </div>
                 </div>
-                <div className='AdminProfile__dataBlock'>
-                    <div className='AdminProfile__left_dataBlock'>
+                <div className='AgentProfile__dataBlock'>
+                    <div className='AgentProfile__left_dataBlock'>
                         <strong>Current Policies:</strong>
                     </div>
-                    <div className='AdminProfile__right_dataBlock AdminProfile__currentPolicies'>
+                    <div className='AgentProfile__right_dataBlock AgentProfile__currentPolicies'>
                         1
                     </div>
                 </div>
             </div>
-            <div className='AdminProfile__Requests'>
+            <div className='AgentProfile__Requests'>
                 <h3>Requests</h3>
-                <div className='AdminProfile__dataBlock'>
-                    <div className='AdminProfile__left_dataBlock'>
+                <div className='AgentProfile__dataBlock'>
+                    <div className='AgentProfile__left_dataBlock'>
                         <strong>Requested Policy</strong>
                     </div>
-                    <div className='AdminProfile__right_dataBlock'>
+                    <div className='AgentProfile__right_dataBlock'>
                         <strong>Requested by</strong>
                     </div>
                 </div>
-                <div className='AdminProfile__dataBlock'>
-                    <div className='AdminProfile__left_dataBlock policyName'>
-                        <p>LIC</p>
+                <div className='AgentProfile__dataBlock'>
+                    <div className='AgentProfile__requestPolicyInfo'>
+                        <div className='AgentProfile__left_dataBlock policyName'>
+                            <p>LIC</p>
+                        </div>
+                        <div className='AgentProfile__right_dataBlock userName'>
+                            <p>Harshit Singh</p>
+                        </div>
                     </div>
-                    <div className='AdminProfile__right_dataBlock userName'>
-                        <p>Harshit Singh</p>
-                    </div>
+                    <button className='AgentProfile__button'>Verified</button>
+                    <button className='AgentProfile__button'>Not Eligible</button>
                 </div>
-                <div className='AdminProfile__dataBlock'>
-                    <div className='AdminProfile__left_dataBlock policyName'>
-                        <p>Pehli IC</p>
+                <div className='AgentProfile__dataBlock'>
+                    <div className='AgentProfile__requestPolicyInfo'>
+                        <div className='AgentProfile__left_dataBlock policyName'>
+                            <p>Pehli IC</p>
+                        </div>
+                        <div className='AgentProfile__right_dataBlock userName'>
+                            <p>Arsla Bhagat</p>
+                        </div>
                     </div>
-                    <div className='AdminProfile__right_dataBlock userName'>
-                        <p>Arsla Bhagat</p>
-                    </div>
+                    <button className='AgentProfile__button'>Verified</button>
+                    <button className='AgentProfile__button'>Not Eligible</button>
                 </div>
             </div>
         </div>
     );
 }
 
-export default AdminProfile;
+export default AgentProfile;
--- Frontend/ims-frontend/src/pages/BuyPolicy.js
+++ Frontend/ims-frontend/src/pages/BuyPolicy.js
@@ -1,14 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './BuyPolicy.css'
 
 const BuyPolicy = () => {
+    const sumAssurance = 3000000; // in Rs
+    const minTenure = 40;
+    const maxTenure = 60;
+    const minPeriod = 1;
+    const maxPeriod = 3;
+    const [period, setPeriod] = useState(1);
+    const [tenure, setTenure] = useState(minTenure);
     return (
         <div className='BuyPolicy__container'>
             <div className='BuyPolicy__heading'>
-                <h1>Transaction</h1>
+                <h1>Payment Details</h1>
             </div>
             <div className='BuyPolicy__policyName'>
-                <h1></h1>
+                <h3>Pehli Policy</h3>
+            </div>
+            <div className='BuyPolicy__dataBlock BuyPolicy__bottomBorder'>
+                <div className='BuyPolicy__leftDataBlock'>
+                    <strong>Minimum Tenure :</strong>
+                </div>
+                {minTenure} years
+            </div>
+            <div className='BuyPolicy__dataBlock BuyPolicy__bottomBorder'>
+                <div className='BuyPolicy__leftDataBlock'>
+                    <strong>Maximum Tenure :</strong>
+                </div>
+                {maxTenure} years
+            </div>
+            <div className='BuyPolicy__dataBlock BuyPolicy__bottomBorder'>
+                <div className='BuyPolicy__leftDataBlock'>
+                    <strong>Minimum Period :</strong>
+                </div>
+                {minPeriod} months
+            </div>
+            <div className='BuyPolicy__dataBlock BuyPolicy__bottomBorder'>
+                <div className='BuyPolicy__leftDataBlock'>
+                    <strong>Maximum Period :</strong>
+                </div>
+                {maxPeriod} months
+            </div>
+            <div className='BuyPolicy__dataBlock BuyPolicy__premium'>
+                premium: {Math.ceil(sumAssurance*period/(tenure*12))} Rs for every
+                <div className='BuyPolicy__decrement' id='BuyPolicy__decrement'>
+                    <button onClick={()=>{if(period > minPeriod) setPeriod(period-1);}}>-</button>
+                </div>
+                <div>{period}</div>
+                <div className='BuyPolicy__increment' id='BuyPolicy__increment'>
+                    <button onClick={()=>{if(period < maxPeriod) setPeriod(period+1);}}>+</button>
+                </div>
+                month for
+                <div className='BuyPolicy__decrement' id='BuyPolicy__decrement'>
+                    <button onClick={()=>{if(tenure > minTenure) setTenure(tenure-1);}}>-</button>
+                </div>
+                <div>{tenure}</div>
+                <div className='BuyPolicy__increment' id='BuyPolicy__increment'>
+                    <button onClick={()=>{if(tenure < maxTenure) setTenure(tenure+1);}}>+</button>
+                </div>
+                years
             </div>
         </div>
     );
