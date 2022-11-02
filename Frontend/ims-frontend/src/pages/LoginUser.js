5cfc8f5647b685c6094b3693e838b7fea5c911a0
Harsh <84851169+HarshHp13@users.noreply.github.com>
2022-11-02 09:45:01 +0530

    Signup Form

--- Frontend/ims-frontend/src/pages/LoginUser.js
+++ Frontend/ims-frontend/src/pages/LoginUser.js
@@ -14,7 +14,25 @@ function LoginUser() {
   const [password, setPassword] = useState("")
   const [usernameSignup, setUsernameSignup] = useState("")
   const [passwordSignup, setPasswordSignup] = useState("")
-  const navigate=useNavigate()
+  const [fname, setfname] = useState("")
+  const [lname, setlname] = useState("")
+  const [dob, setDob] = useState(null)
+  // const [email, setEmail] = useState("")
+  const [house, setHouse] = useState("")
+  const [street, setStreet] = useState("")
+  const [city, setCity] = useState("")
+  const [state, setState] = useState("")
+  const [zipcode, setZipcode] = useState("")
+  const [profession, setProfession] = useState("")
+  const [income, setIncome] = useState("")
+  // const [referrals, setReferrals] = useState(0)
+  const [gender, setGender] = useState("")
+  const [maritalStatus, setMaritalStatus] = useState("")
+  const [signUpDate, setSignUpDate] = useState(null)
+  // const [referred, setReferred] = useState("")
+  // const [branchId, setBranchId] = useState("")
+  const [phone, setPhone] = useState("")
+  const navigate= useNavigate()
 
   const userLogin=async (username, password)=>{
     const auth={
@@ -27,7 +45,6 @@ function LoginUser() {
           'Content-Type': "application/x-www-form-urlencoded",
       }
     });
-    console.log(username+" : "+password)
     return res;
   }
 
@@ -35,25 +52,38 @@ function LoginUser() {
     e.preventDefault();
     userLogin(user,pass)
     .then((res)=>{
-      try{
-        const accessToken=res.headers.get('access_token')
-        const refreshToken=res.headers.get('refresh_token')
-        setAuth({accessToken, refreshToken})
-        navigate('/')
-      }
-      catch(e){
-        console.log(e)
-        alert("Wrong Username or Password!!")
-      }
+      const accessToken=res.headers.get('access_token')
+      const refreshToken=res.headers.get('refresh_token')
+      setAuth({accessToken, refreshToken})
+      document.elementFromPoint(0,0).click()
+      
+    })
+    .catch((error)=>{
+      console.log(error)
+      alert("Wrong Username or Password!!")
     })
     
   }
 
 
-  const userSignup= async (username, password)=>{
+  const userSignup= async ()=>{
     const auth={
-        username: username,
-        password: password
+      username: usernameSignup,
+      password: passwordSignup,
+      fname: fname,
+      lname: lname,
+      // date: dob,
+      house: house,
+      street: street,
+      city: city,
+      state: state,
+      zipcode: zipcode,
+      income: income,
+      profession: profession,
+      gender: gender,
+      maritalStatus: maritalStatus,
+      phone: phone
+
     }
     const data=qs.stringify(auth)
     const res = await axios.post('/signUp', data, {
@@ -62,7 +92,7 @@ function LoginUser() {
         }
     })
     // .then(userLogin(username,password))
-    console.log(username+" : "+password)
+    console.log(usernameSignup+" : "+passwordSignup)
     return res;
  }
 
@@ -101,19 +131,31 @@ function LoginUser() {
 
   if(signUp){
     return (
-      <>
+      <><div className='signUpUser__wrapper'>
+
+      
           <form className='signUpUser__container'>
 
             <center><h3>User Sign Up</h3></center>
-            <Input className='loginUser__input' type='text' placeholder='First Name' />
-            <Input className='loginUser__input' type='text' placeholder='Last Name' />
-            <Input className='loginUser__input' type='text' placeholder='Username' onChange={(e)=>setUsernameSignup(e.target.value)} required/>
+            <Input className='loginUser__input' type='email' placeholder='Email' onChange={(e)=>setUsernameSignup(e.target.value)} required/>
             <Input className='loginUser__input' type='password' placeholder='Password' onChange={(e)=>setPasswordSignup(e.target.value)} required/> 
-            <Input className='loginUser__input' type='email' placeholder='Email' />
-            <Input className='loginUser__input' type='tel' placeholder='Phone' />
+            <Input className='loginUser__input' type='text' placeholder='First Name' onChange={(e)=>setfname(e.target.value)} required />
+            <Input className='loginUser__input' type='text' placeholder='Last Name' onChange={(e)=>setlname(e.target.value)} required />
+            <Input className='loginUser__input' type='date' placeholder='DOB' onChange={(e)=>setDob(e.target.value)} required />
+            <Input className='loginUser__input' type='text' placeholder='House No' onChange={(e)=>setHouse(e.target.value)} required />
+            <Input className='loginUser__input' type='text' placeholder='Street Name' onChange={(e)=>setStreet(e.target.value)} required />
+            <Input className='loginUser__input' type='text' placeholder='City' onChange={(e)=>setCity(e.target.value)} required />
+            <Input className='loginUser__input' type='text' placeholder='State' onChange={(e)=>setState(e.target.value)} required />
+            <Input className='loginUser__input' type='text' placeholder='Zipcode' onChange={(e)=>setZipcode(e.target.value)} required />
+            <Input className='loginUser__input' type='number' placeholder='Income' onChange={(e)=>setIncome(e.target.value)} required />
+            <Input className='loginUser__input' type='text' placeholder='Profession' onChange={(e)=>setProfession(e.target.value)} required />
+            <Input className='loginUser__input' type='text' placeholder='Gender' onChange={(e)=>setGender(e.target.value)} required />
+            <Input className='loginUser__input' type='text' placeholder='Marital Status' onChange={(e)=>setMaritalStatus(e.target.value)} required />
+            <Input className='loginUser__input' type='tel' placeholder='Phone' onChange={(e)=>setPhone(e.target.value)} required />
             <center><Button className='loginUser__button' onClick={clickHandlerSignup}>Sign Up</Button></center>
             <center><div className='loginUser__text' onClick={()=>setSignUp(false)}>Already a user? Sign In</div></center>
           </form>
+          </div>
       </>
     )
   }
